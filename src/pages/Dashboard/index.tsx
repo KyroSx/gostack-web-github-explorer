9b77d7404415827823eb77fd49ae3a38591d48b6
leonardo <leolub150@gmail.com>
2020-06-29 11:52:56 -0300

    create home page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,49 @@
 import React from "react";
+import { FiChevronRight } from "react-icons/fi";
 
-import { Title } from "./styles";
+import { Title, Form, SearchButton, SearchInput, Repositories } from "./styles";
+import logo from "../../assets/logo.svg";
 
 const Dashboard: React.FC = () => {
-  return <Title>Dashboard</Title>;
+  return (
+    <>
+      <img src={logo} alt="Github logo" />
+      <Title>Explore repositórios no GitHub</Title>
+
+      <Form>
+        <SearchInput placeholder="Digite aqui" />
+        <SearchButton>Pesquisar</SearchButton>
+      </Form>
+
+      <Repositories>
+        <a href="test">
+          <img
+            src="https://avatars3.githubusercontent.com/u/33635656?s=460&u=6146d983b3677cca68dfc59c01f9b37acd9012b6&v=4"
+            alt=""
+          />
+          <div>
+            <strong>Titulo</strong>
+            <p>Descrição</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="test">
+          <img
+            src="https://avatars3.githubusercontent.com/u/33635656?s=460&u=6146d983b3677cca68dfc59c01f9b37acd9012b6&v=4"
+            alt=""
+          />
+          <div>
+            <strong>Titulo</strong>
+            <p>Descrição</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
